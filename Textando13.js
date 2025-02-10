fbf2653807608ee1bf19b3434c5cc2c4ec16fdff
Juan Reno <142558689+EuuJuan@users.noreply.github.com>
2025-02-10 18:29:39 -0300

    Add files via upload

---
+++ Textando13.js
@@ -0,0 +1,38 @@
+/*
+torrar("Pão integral", "Gustavo");
+
+function torrar(pao, nome = "Cliente", valor = 100){
+    console.log("torrada feita com " + pao);
+    console.log("Pedido de " + nome);
+    console.log("O Valor total é " + valor);
+}
+*/
+/*
+createStringConnection("db_product", "Felipe", "1102");
+
+function createStringConnection(databaseName, user, pass){
+    console.log(`Connect:DBCONNECT; user=${user};pass=${pass};intial_database = ${databaseName} `);
+}
+*/
+/*
+let resultado = soma(5, 10);
+
+console.log("O Resultado dessa função é " + resultado);
+
+function soma(n1, n2){
+    let somatorio = n1 + n2;
+    return somatorio;
+}
+*/
+/*
+let userName = getFirstName("Jose-Silveira-Costa", "-");
+console.log("Seja bem-vindo " + userName);
+
+userName = getFirstName("Gustavo Matos Mendrot", " ");
+console.log("Seja bem-vindo " + userName);
+
+function getFirstName(name, splitChar){
+    let firstName = name.split(splitChar)[0];
+    return firstName;
+}
+*/
